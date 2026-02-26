709160e8603985d85099a3a985314e2d09e7ec51
Anthony Perez <anthonyj.perez@gmail.com>
2026-02-26 16:13:04 -0500

    Initializes C scripting compatibility layer

--- c_src/sl_compat.c
+++ c_src/sl_compat.c
@@ -5,7 +5,39 @@
  * in scripting.h are compiled away and never produce symbols.
  */
 #include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
 #include "scripting.h"
+#include "core.h"
+#include "net_crypt.h"
+#include "session.h"
+#include "map_parse.h"
+#include "clan_db.h"
+#include "config.h"
+#include "db_mysql.h"
+
+/* -------------------------------------------------------------------------
+ * Functions previously in scripting.c that are referenced as callbacks or
+ * used by sl_g_* wrappers.
+ * --------------------------------------------------------------------- */
+
+int sl_throw(struct block_list *bl, va_list ap) {
+    USER *sd = NULL;
+    nullpo_ret(0, sd = (USER *)bl);
+    char *buf = va_arg(ap, char *);
+    int len = va_arg(ap, int);
+    if (!rust_session_exists(sd->fd) || rust_session_get_eof(sd->fd)) {
+        rust_session_set_eof(sd->fd, 8);
+        return 0;
+    }
+    WFIFOHEAD(sd->fd, len);
+    memcpy(WFIFOP(sd->fd, 0), buf, len);
+    WFIFOSET(sd->fd, encrypt(sd->fd));
+    return 0;
+}
+
+/* sl_updatepeople is provided as a Rust #[no_mangle] symbol in ffi/scripting.rs */
 
 int sl_doscript_blargs(char *root, const char *method, int nargs, ...) {
     struct block_list *args[16] = {0};
